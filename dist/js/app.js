2e2ead4b2ea939ca4b365e13a4074acb604d20f9
Don <don@Dons-MacBook-Air.local>
2018-06-10 01:32:38 +0800

    project structure setup

---
+++ dist/js/app.js
@@ -0,0 +1,77 @@
+// Table of Contents
+//
+/***
+.login-form
+#publisherOnboarding
+#publisherOnboardingSuccess
+#publisherDashboard
+
+
+#nodeOnboarding
+#nodeDashboard
+
+
+
+
+***/
+
+$(document).ready(function(){
+})
+
+
+// Land on website and register
+$(document).on("click", '#registerPublisher', function(){
+	$('.landing-page--container').fadeOut();
+	setTimeout(function(){
+		$('.app--container').fadeIn();
+		$('#publisherOnboarding').fadeIn();
+	}, 300)
+})
+
+
+// Add Item on website input to group
+$('#websiteInput').focus(function(){
+	$(this).val('www.hexcapitalgroup.com');
+})
+
+$('#addWebsite').on("click", $(this), function(){
+	$('#addWebsite').attr('disabled', 'true')
+	$('#websiteGroup').append('<li>' + 'www.hexcapitalgroup.com' + '</li>');
+	$('#websiteInput').val('');
+})
+
+
+// MOVE TO NEXT STEP (SUCCESS -> DASHBOARD)
+
+$('#publisherOnboardingNext').on("click", $(this), function(){
+	$('#publisherOnboarding').fadeOut();
+
+	setTimeout(function(){
+		$('#publisherOnboardingSuccess').fadeIn();
+
+		setTimeout(function(){
+			$('.success--callout').addClass("appear");	
+
+			setTimeout(function(){
+				$('.success--callout').removeClass("appear");				
+			}, 2000)
+
+		}, 300);
+	}, 300);
+})
+
+
+// HIDE PUBLISHER, SHOW THE NODES
+
+
+// SPLIT THE SCREEN
+
+$('#splitScreen').on("click", $(this), function(){
+	$(this).fadeOut();
+	$('.main--app-container').addClass("split-screen");
+	$('.node-exp--container').fadeIn();
+})
+
+// $(document).keypress(function(e) {
+// 	alert("hello");
+// }
