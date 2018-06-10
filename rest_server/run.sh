e7222596588fb2684c6d17598fb059dbed6ffa80
rafal <rafal@rafal.mbp>
2018-06-10 10:16:19 +0800

    added /serveurl

--- rest_server/run.sh
+++ rest_server/run.sh
@@ -1,2 +1,2 @@
-cleos wallet unlock -n rafal --password PW5J5cXz33Ri3q5n8cgNirZ5r8GrwHN2RA2aebrPDfn78wxgcBEu
+cleos wallet unlock -n rafal --password PW5J5cXz33Ri3q5n8cgNirZ5r8GrwHN2RA2aebrPDfn78wxgcBEub --unlock-timeout 30000
 node server.js
